c55cdfd574e59645063c8224318146893d1466c9
GregoryNikolaishvili <gianick68@gmail.com>
2024-01-19 23:34:14 +0400

    Added SwaggerGenOptionsExt.AddAllDomainPrimitivesSwaggerMappings

--- src/AltaSoft.DomainPrimitives/AssemblyAttributes.cs
+++ src/AltaSoft.DomainPrimitives/AssemblyAttributes.cs
@@ -1,3 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
+/// <summary>
+/// Specifies that types that are normally not visible to the assembly are visible to the specified assembly for testing purposes.
+/// </summary>
 [assembly: InternalsVisibleTo("AltaSoft.DomainPrimitives.XmlDataTypes.Tests")]
---
+++ src/AltaSoft.DomainPrimitives/SwaggerGenServiceCollectionExt.cs
@@ -0,0 +1,34 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AltaSoft.DomainPrimitives;
+
+/// <summary>
+/// A static class providing methods to configure Swagger mappings for DomainPrimitive types.
+/// </summary>
+public static class SwaggerGenOptionsExt
+{
+	/// <summary>
+	/// Adds Swagger mappings for all DomainPrimitive types to the specified SwaggerGenOptions.
+	/// </summary>
+	/// <param name="options">The SwaggerGenOptions to which the mappings will be added.</param>
+	public static void AddAllDomainPrimitivesSwaggerMappings(this SwaggerGenOptions options)
+
+	{
+		// Retrieves all loaded assemblies in the current AppDomain.
+		var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+		// Retrieves all types from all loaded assemblies that have a static SwaggerTypeHelper.AddSwaggerMappings method.
+		var typesWithAddSwaggerMappings = loadedAssemblies
+			.SelectMany(assembly => assembly.GetExportedTypes().Where(type => type.IsPublic && type.Name == "SwaggerTypeHelper"))
+			.Select(type => type.GetMethod("AddSwaggerMappings", BindingFlags.Public | BindingFlags.Static));
+
+		// Calls the AddSwaggerMappings method for each type.
+		foreach (var method in typesWithAddSwaggerMappings)
+		{
+			method?.Invoke(null, [options]);
+		}
+	}
+}
