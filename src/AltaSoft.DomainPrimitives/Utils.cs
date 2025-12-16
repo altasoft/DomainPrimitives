5b21073fbfec120300cc5bfb8048342bde9dc55f
Temo Nikolaishvili <t.nikolaishvili@altasoft.ge>
2025-12-16 16:49:10 +0400

    Datetime unspecified used

--- src/AltaSoft.DomainPrimitives/Utils.cs
+++ src/AltaSoft.DomainPrimitives/Utils.cs
@@ -14,12 +14,12 @@ public static class Utils
     /// </summary>
     /// <param name="value">The DateOnly value to convert.</param>
     /// <returns>A DateTime representation of the given DateOnly value.</returns>
-    public static DateTime ToDateTime(this DateOnly value) => value.ToDateTime(TimeOnly.MinValue, DateTimeKind.Local);
+    public static DateTime ToDateTime(this DateOnly value) => value.ToDateTime(TimeOnly.MinValue, DateTimeKind.Unspecified);
 
     /// <summary>
     /// Converts a TimeOnly value to a DateTime, with the date component set to the minimum value and kind set to local.
     /// </summary>
     /// <param name="value">The TimeOnly value to convert.</param>
     /// <returns>A DateTime representation of the given TimeOnly value.</returns>
-    public static DateTime ToDateTime(this TimeOnly value) => new(value.Ticks, DateTimeKind.Local);
+    public static DateTime ToDateTime(this TimeOnly value) => new(value.Ticks, DateTimeKind.Unspecified);
 }
